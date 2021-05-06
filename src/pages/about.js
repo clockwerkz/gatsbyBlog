5121e0b39de27a82a57cbf49df1591647031836e
clockwerkz <clockwerkz@gmail.com>
2021-05-05 23:40:59 -0700

    adds header and footer

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Footer from '../components/footer';
+import Header from '../components/header';
+
 const AboutPage = () => {
     return (
         <div>
+            <Header />
             <h1>About Page</h1>
             <p>I currently tutor on the Wyzant Platform.</p>
             <p>Interested in working with me? <Link to="/contact">Contact me.</Link></p>
+            <Footer />
         </div>
     )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,15 @@
 import * as React from "react"
 import { Link } from "gatsby";
-
+import Footer from '../components/footer';
+import Header from '../components/header';
 const IndexPage = () => {
   return (
    <div>
+     <Header />
      <h1>Hello.</h1>
      <p>I'm Carlos, a full-stack developer living in Irvine, California.</p>
      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
+     <Footer />
    </div>
   )
 }
