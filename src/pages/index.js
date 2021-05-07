b232b20438d4426d71cc39662135aec04f88f3fe
Carlos <clockwerkz@gmail.com>
2021-05-06 19:29:33 -0700

    adds a layout component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
 import * as React from "react"
 import { Link } from "gatsby";
-import Footer from '../components/footer';
-import Header from '../components/header';
+import Layout from "../components/layout";
 const IndexPage = () => {
   return (
-   <div>
-     <Header />
+   <Layout>
      <h1>Hello.</h1>
      <p>I'm Carlos, a full-stack developer living in Irvine, California.</p>
      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
-     <Footer />
-   </div>
+   </Layout>
   )
 }
 
