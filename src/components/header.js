a26c154b619d461e13ab54de8070bf9dc87d03c3
clockwerkz <clockwerkz@gmail.com>
2021-05-06 23:28:12 -0700

    adds scss modules

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Link } from "gatsby";
 
+//import "./header.module.scss";
+import * as headerStyles from "./header.module.scss";
+
 const Header = () => {
     return (
         <header>
             <h2>Carlos' Site</h2>
             <nav>
                 <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About Carlos</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/" className={headerStyles.link}>Home</Link></li>
+                    <li><Link to="/blog" className={headerStyles.link}>Blog</Link></li>
+                    <li><Link to="/about" className={headerStyles.link}>About Carlos</Link></li>
+                    <li><Link to="/contact" className={headerStyles.link}>Contact</Link></li>
                 </ul>
             </nav>
         </header>
